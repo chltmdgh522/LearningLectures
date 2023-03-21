a3c0e26c151727170e274dd76126565130e3a01d
chltmdgh522 <chltmdgh517@naver.com>
2023-03-21 16:22:13 +0900

    학교에서 공부한 예제

---
+++ SchoolJava/src/java3/OverflowUnderflowExample.java
@@ -0,0 +1,17 @@
+package java3;
+
+public class OverflowUnderflowExample {
+    public static void main(String[] args) {
+        byte var1 = 125;
+        for (int i = 0; i < 5; i++) {
+            var1++;
+            System.out.println("i = " + i);
+        }
+        System.out.println("----------------------------");
+        byte var2 = -125;
+        for (int i = 0; i < 5; i++) {
+            var2--;
+            System.out.println(" var2 = " + var2);
+        }
+    }
+}
