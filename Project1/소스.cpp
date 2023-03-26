c6be57359acb613801214bed34ec7185291337b8
chltmdgh522 <chltmdgh517@naver.com>
2023-03-26 19:26:03 +0900

    and 와   or

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,124 +1,34 @@
 #include <stdio.h>
-#include <iostream>
 #include <stdlib.h>
-#include <conio.h>
-#include <string.h>
-#include <math.h>
-#define _CRT_SECURE_NO_WARNINGS
-#define STACK_SIZE 10
-int main(void) {
-#include <stdio.h>
-#include <stdlib.h>
-#define STACK_SIZE 10
-
-	int* stack;
-	int top = -1;               // top 초기화
-	int size = 1;
-
-	int isEmpty() {
-		if (top == -1) return 1;
-		else return 0;
+void insert(int** arr, int* size, int index, int value) {
+	*arr = (int*)realloc(*arr, (*size + 1) * sizeof(int));  // 메모리 크기 확잧
+	for (int i = *size; i > index; i--) {  // 인덱스 이후 요소들을 한 칸씩 뒤로 이동
+		(*arr)[i] = (*arr)[i - 1];
 	}
-
-	int pop() {
-		if (isEmpty()) {	// 스택이 공백 상태인 경우
-			printf("\n\n Stack is Empty!!\n");
-			return 0;
-		}
-		else
-		{
-			// 동적 배열로 구현
-			int i, re, * temp;
-
-			if (top % 10 == 0)
-			{
-				printf(" CHANGE STACK\n");
-				re = stack[top];
-				temp = (int*)malloc(STACK_SIZE * (size - 1) * sizeof(int));
-				for (i = 0; i < top; i++) temp[i] = stack[i];
-
-				free(stack);
-
-				stack = (int*)malloc(STACK_SIZE * (size - 1) * sizeof(int));
-
-				for (i = 0; i < top; i++) stack[i] = temp[i];
-
-				free(temp);
-
-				top--;
-				size--;
-
-				return re;
-
-			}
-			else
-				return stack[top--];	      // 현재 top의 원소를 삭제한 후 top 감소
-		}
-	}
-
-	// 스택이 포화 상태인지 확인하는 연산
-	int isFull() {
-		if (top % STACK_SIZE == 9) return 1;
-		else return 0;
-	}
-
-	// 스택의 top에 원소를 삽입하는 연산
-	void push(int item) {
-		int* temp;
-
-		if (isFull()) {
-			printf(" Full STACK\n");
-			temp = (int*)malloc((STACK_SIZE * size) * sizeof(int));
-
-			for (int i = 0; i < (top + 1); i++) temp[i] = stack[i];
-
-			free(stack);
-
-			stack = (int*)malloc((STACK_SIZE * (size + 1)) * sizeof(int));
-
-			for (int i = 0; i < (top + 1); i++) stack[i] = temp[i];
-			stack[++top] = item;  // top을 증가시킨 후 현재 top에 원소 삽입
-
-			size++;
-
-			free(temp);
-
-		}
-		else stack[++top] = item;  // top을 증가시킨 후 현재 top에 원소 삽입
-	}
-
-	// 스택의 원소를 출력하는 연산
-	void printStack() {
-		int i;
-		printf(" STACK SIZE [%d]\n", (STACK_SIZE * size));
-		printf("\n STACK [ ");
-		for (i = 0; i <= top; i++) printf("%d ", stack[i]);
-		printf("]\n");
-	}
-
-	int main(void) {
-		int i;
-		stack = (int*)malloc(STACK_SIZE * sizeof(int));
-
-		for (i = 0; i < 44; i++) push(i + 1);
-
-		printStack();
-
-		for (i = 0; i < 7; i++) printf(" POP Data [%d]\n", pop());
-
-		printStack();
-
-		free(stack);
-
-		getchar();
-	}
+	(*arr)[index] = value;  // 인덱스 위치에 새로운 값 추가
+	*size += 1;  // 배열 크기 증가
 }
 
+int main() {
+	int* arr = (int*)malloc(5 * sizeof(int));  // 5개의 int형 배열 할당
+	int size = 5;  // 배열 크기 초기화
 
+	// 배열 초기화
+	for (int i = 0; i < size; i++) {
+		arr[i] = i + 1;
+	}
 
+	
+	insert(&arr, &size, 2, 7);  //insert함수 추가 및 인덱스 2에 값 10 삽입
+	printf("chage array: ");
+	for (int i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 
-
-
+	free(arr);
+	return 0;
+}
 
 
 
