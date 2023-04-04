d289e98d56667db923f1e57a52890c27ea1640f0
chltmdgh522 <chltmdgh517@naver.com>
2023-04-04 15:50:04 +0900

    학교에서 공부한 예제ㄷ(switch)

---
+++ SchoolJava/src/java5/SwitchCharExample.java
@@ -0,0 +1,22 @@
+package java5;
+
+public class SwitchCharExample {
+    public static void main(String[] args) {
+        char grade = 'B';
+
+        switch (grade) {
+            case 'A':
+            case 'a':
+                System.out.println("우수회원입니다.");
+                break;
+            case 'B':
+            case 'b':
+                System.out.println("일반회원입니다.");
+                break;
+            default:
+                System.out.println("그냥손님입니다.");
+
+        }
+
+    }
+}
