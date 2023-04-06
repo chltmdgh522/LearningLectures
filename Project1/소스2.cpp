a8cecce77afc9f3a280c982a407d5a4c63a59049
chltmdgh522 <chltmdgh517@naver.com>
2023-04-06 22:51:10 +0900

    학교과제

--- Project1/소스2.cpp
+++ Project1/소스2.cpp
@@ -1,42 +1,53 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
 int main(void) {
+	//fputs("dfsd",fp);
 
 	FILE* fp;
 	int num[5];
-	int data[20];
-	int max = 0; 
+	int data[5];
+	int max = 0;
+	int md = 0;
 	int index = 0;
 
 	fp = fopen("data.txt", "w");
-	for (int i = 0; i < 20; i++) {
-		scanf("%d",&num[i]);
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &num[i]);
 		fprintf(fp, "%d\n", num[i]);
 	}
 	fclose(fp);
 	printf("===================================\n");
 	int a = 0;
 	fp = fopen("data.txt", "r");
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 5; i++) {
 		fscanf(fp, "%d", &data[i]);
 	}
 
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 5; i++) {
 		if (max < data[i]) {
 			max = data[i];
 			index = i;
 		}
-	}	
-	
+	}
+	for (int i = 0; i < 5; i++) {
+		if (max> data[i]) {
+			if (md < data[i]) {
+				md = data[i];
+			}
+			
+		}
+	}
+
 	fp = fopen("dataOut.txt", "w");
-	fprintf(fp, "Max 값: %d\n", max);
+	fprintf(fp, "max값: %d\nindex 값: %d\n", max,index);
+	fprintf(fp, "md값: %d", md);
 	fclose(fp);
 
-	
-	printf("max값: %d\nindex 값: %d\n", max,index);
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", data[i]);
+	}
+
+	printf("max값: %d\nindex 값: %d\n", max, index);
+	printf("md값 %d", md);
 	fclose(fp);
-
-
-
-
-}*/
+}
