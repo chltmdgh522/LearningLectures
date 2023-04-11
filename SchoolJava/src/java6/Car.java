5341eac3529dbb4d11a1b628f0961a194ed26421
chltmdgh522 <chltmdgh517@naver.com>
2023-04-11 18:14:31 +0900

    학교에서 공부한 예제

---
+++ SchoolJava/src/java6/Car.java
@@ -0,0 +1,26 @@
+package java6;
+
+public class Car {
+
+    String company = "현대자동차";
+    String model;
+    String color;
+
+
+    Car() {
+
+    }
+    Car(String model) {
+        this.model = model;
+    }
+}
+
+class CarExample {
+
+    public static void main(String[] args) {
+        Car car1 = new Car("d");
+        Car car2 = new Car();
+
+        car1.
+    }
+}
