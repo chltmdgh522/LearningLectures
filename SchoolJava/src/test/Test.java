7bc6f83cbfe28e73504884f210ea47d92d81016b
chltmdgh522 <chltmdgh517@naver.com>
2023-04-25 22:27:59 +0900

    예제 문제 test

--- SchoolJava/src/test/Test.java
+++ SchoolJava/src/test/Test.java
@@ -4,11 +4,7 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        int a = (int) ((Math.random() * 6) + 1);
-        Random rd= new Random();
-        int b =rd.nextInt(6)+1;
-        System.out.println(a);
 
-
+        System.out.println((double)(85/2));
     }
 }
