cc3a0121772082856341d37734d5fc7720fb6c01
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2023-04-30 22:00:03 +0900

    Update RouletteController.cs
    룰렛 회전속도 멈추는 법 증가하는 법 설정(9주차)

--- 룰렛게임/RouletteController.cs
+++ 룰렛게임/RouletteController.cs
@@ -15,11 +15,12 @@ public class RouletteController : MonoBehaviour
     void Update()
     {
         //클릭하면 회전속도를 설정한다.
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0)) { //마우스를 눌렀을떄 
             this.rotSpeed = 10; //양수면 반시계방향
         }
         //회전속도만큼 룰렛 회전
         transform.Rotate(0, 0, this.rotSpeed);
+        this.rotSpeed * 0.96f;
         
     }
 }
