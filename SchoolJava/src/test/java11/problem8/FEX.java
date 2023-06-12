026369dd073842c57cbf13e8a8a2e25820909248
seung_ho_choi.s <chltmdgh517@naver.com>
2023-06-12 22:43:11 +0900

    .

--- SchoolJava/src/test/java11/problem8/FEX.java
+++ SchoolJava/src/test/java11/problem8/FEX.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class FEX {
     public static void main(String[] args) {
         try(FileWriter fw=new FileWriter("csh")){
-         fw.write("gdgd");
+         fw.write("gd");
         } catch (IOException e) {
             System.out.println("dd");
         }
