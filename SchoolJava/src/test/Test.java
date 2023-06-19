0f96f92d748684870e43b792b509254ed25d9142
seung_ho_choi.s <chltmdgh517@naver.com>
2023-06-19 12:48:08 +0900

    .

--- SchoolJava/src/test/Test.java
+++ SchoolJava/src/test/Test.java
@@ -7,8 +7,8 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        try{
-            Test1 test1=new Test1("승호");
+        try( Test1 test1=new Test1("승호")){
+
             test1.write("d");
         }catch(Exception e){
             System.out.println(e);
