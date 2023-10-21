ebcc30ba6fdf57f063d5bddb1c943e340b21f589
seung_ho_choi.s <chltmdgh517@naver.com>
2023-10-21 22:09:57 +0900

    중간시험 연습

---
+++ softClass/examMiddle.py
@@ -0,0 +1,34 @@
+# import re
+# import os
+
+# print(os.getcwd())
+
+# os.chdir(r"C:\Users\chltm\Github")
+# print(os.getcwd())
+# print(os.listdir())
+
+# with open("a.txt", "w") as f:
+#     f.write("gd")
+
+# number = "sadfsadf 4545445-655545-sadfsadf878787"
+# a = re.findall(r"\d{6}", number)
+# print(a)
+
+
+import re
+# example = "a:저는 91년에 태어났습니다. 97년에는 승호가 있습니다. 지금은 2023년입니다ㅏ. b:dfasdfasdfasdf a:dasdfasdfasdfsdfsd"
+
+f = open("a.txt", "r")
+ss = f.readlines()
+print(ss)
+
+# Line = set(re.findall(r'a:+', ss))
+# print(Line)
+
+# cc = "rachel:"
+# print(cc[-1:])
+
+# list = []
+# for i in range(2):
+#     list += ["dd"]
+# print(list)
