f9f5aae9d3c2f4bc0b4798e550ed2bbe70661f02
seung_ho_choi.s <chltmdgh517@naver.com>
2023-12-13 22:21:57 +0900

    test

--- softClass/week7.py
+++ softClass/week7.py
@@ -1,72 +1,72 @@
-# import re
-# import csv
-# import os
+import re
+import csv
+import os
 
-# import user1
+import user1
 
-# # # 모듈로 불러오기
-# # os.chdir(r"C:\Users\chltm\Desktop\파이썬")
-# # total = user1.opencsv("pop.csv")
-# # for i in total[:5]:
-# #     print(i)
+# # 모듈로 불러오기
+# os.chdir(r"C:\Users\chltm\Desktop\파이썬")
+# total = user1.opencsv("pop.csv")
+# for i in total[:5]:
+#     print(i)
 
-# i = ["123!!", "151,767", "11,093", "27,394", '']
-# for j in i:
-#     try:
-#         if re.search(r'[a-z가-힣!]', j):
-#             i[i.index(j)] = j
-#         else:
-#             i[i.index(j)] = float(re.sub(r',', '.', j))
+i = ["123!!", "151,767", "11,093", "27,394", '']
+for j in i:
+    try:
+        if re.search(r'[a-z가-힣!]', j):
+            i[i.index(j)] = j
+        else:
+            i[i.index(j)] = float(re.sub(r',', '.', j))
 
-#     except:
-#         pass
+    except:
+        pass
 
-# # #모듈 관리
-# # import user1
-# # os.chdir(r"C:\Users\chltm\Desktop\파이썬")
-# # a = [["국어", "수학"], ["87", "55"]]
-# # user1.writecsv("test.csv", a)
-
-
-# # os.chdir(r"C:\Users\chltm\Desktop\파이썬")
-# # f = open("a.csv", "r", encoding="UTF-8")
-
-# # new = csv.reader(f)
-
-# # for i in new:
-# #     print(i)
-
-# # a_list = []
-# # for i in new:
-# #     a_list.append(i)
-# # print(a_list)
-
-# # f.close()
+# 모듈 관리
+os.chdir(r"C:\Users\chltm\Desktop\파이썬")
+a = [["국어", "수학"], ["87", "55"]]
+user1.writecsv("test.csv", a)
 
 
-# print(user1.opencsv("a.csv"))
+os.chdir(r"C:\Users\chltm\Desktop\파이썬")
+f = open("a.csv", "r", encoding="UTF-8")
 
-# # print(opencsv("a.csv"))
-# # a = [["성명", "파이썬", "빅데이터"], ["D", 80, 90], [
-# #     "C", 100, 98], ["B", 98, 88], ["A", 88, 74]]
+new = csv.reader(f)
 
+for i in new:
+    print(i)
 
-# # f = open("abc.csv", "w", newline="")
-# # csvObject = csv.writer(f, delimiter=',')
-# # csvObject.writerows(a)
-# # f.close()
+a_list = []
+for i in new:
+    a_list.append(i)
+print(a_list)
 
-
-# # def writecsv(filename, the_list):
-# #     f = open(filename, "w", newline="")
-# #     csvob = csv.writer(f, delimiter=",")
-# #     csvob.writerows(the_list)
-# #     f.close()
-# #     return csvob
+f.close()
 
 
-# # writecsv("a2.csv", a)
+print(user1.opencsv("a.csv"))
 
-total = []
-total.append(1)
+# print(opencsv("a.csv"))
+# a = [["성명", "파이썬", "빅데이터"], ["D", 80, 90], [
+#     "C", 100, 98], ["B", 98, 88], ["A", 88, 74]]
+
+
+# f = open("abc.csv", "w", newline="")
+# csvObject = csv.writer(f, delimiter=',')
+# csvObject.writerows(a)
+# f.close()
+
+
+# def writecsv(filename, the_list):
+#     f = open(filename, "w", newline="")
+#     csvob = csv.writer(f, delimiter=",")
+#     csvob.writerows(the_list)
+#     f.close()
+#     return csvob
+
+
+writecsv("a2.csv", a)
+
+total = [1, 2, 3]
+total += [4]
 print(total)
+print(total.index(1))
