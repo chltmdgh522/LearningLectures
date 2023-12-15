5eda3808c814fe7ca6229b2ca4386a8221f2db86
seung_ho_choi.s <chltmdgh517@naver.com>
2023-12-15 21:39:00 +0900

    test

--- 딥러닝/week3.py
+++ 딥러닝/week3.py
@@ -5,9 +5,13 @@ import numpy as np
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-digit = train_images[4]
+digit = train_images[0]
 plt.imshow(digit, cmap=plt.cm.binary)
 plt.show()
 
 my_slice = train_images[10:100]
 print(my_slice.shape)
+
+print(train_images.shape)
+
+print(train_labels)
