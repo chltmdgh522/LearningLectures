055b71d8877d9c3ed94bfca8785919aa2728c810
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2024-04-25 00:02:26 +0900

    Update 메모장.cpp

--- WindowProgramming/중간고사/메모장.cpp
+++ WindowProgramming/중간고사/메모장.cpp
@@ -38,13 +38,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
     }
     return 0;
 }
-
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
     HDC hdc;
     PAINTSTRUCT ps;
     static char str[10][100]; //모든 행에 대한 문자를 저장하는 것 str[0] 첫번째 행에 대한 문자열
-    static int count, line, pos; // pos 변수 추가하여 현재 커서 위치 저장
+    static int count, line;
     static SIZE size;
     int i;
     switch (iMsg)
@@ -54,7 +53,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
         ShowCaret(hwnd);
         count = 0;
         line = 0;
-        pos = 0; // 초기 커서 위치는 0으로 설정
         break;
     case WM_PAINT:
         hdc = BeginPaint(hwnd, &ps);
@@ -62,12 +60,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
         {
             GetTextExtentPoint(hdc, str[i], strlen(str[i]), &size);
             TextOut(hdc, 0, i * 20, str[i], strlen(str[i]));
+            SetCaretPos(size.cx, i * 20);
         }
-        SetCaretPos(size.cx, pos * 20); // 커서 위치 설정
         EndPaint(hwnd, &ps);
         break;
     case WM_CHAR:
-        if (count > 99)
+        if (count > 10)
         {
             MessageBox(NULL, "더 이상 입력 할 수 없습니다.", "경고", MB_OK);
             count--;
@@ -79,7 +77,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
             {
                 line--;
                 count = strlen(str[line]);
-                pos = line; // 라인이 변경될 때 커서도 함께 이동
             }
             if (count == 0 && line == 0)
             {
@@ -92,53 +89,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
             {
                 count = 0;
                 line++;
-                pos = line; // 엔터키 눌렀을 때 커서 이동
             }
             else
             {
                 MessageBox(NULL, "10번이상 Enter금지.", "Warning", MB_OK);
             }
         }
-        else if (pos <= line) // 현재 라인보다 높은 라인에서는 입력하지 않음
-        {
-            if (count < strlen(str[pos])) // 커서 위치부터 입력이 시작됨
-            {
-                for (i = strlen(str[pos]); i >= count; i--)
-                {
-                    str[pos][i + 1] = str[pos][i];
-                }
-                str[pos][count] = wParam;
-                count++;
-            }
-            else
-            {
-                str[pos][count++] = wParam;
-            }
-            str[pos][count] = '\0';
-        }
+        else str[line][count++] = wParam;
+        str[line][count] = '\0';
         InvalidateRgn(hwnd, NULL, TRUE);
-        break;
-    case WM_KEYDOWN: // 키보드 입력 처리
-        switch (wParam)
-        {
-
-        case VK_UP: // 위 화살표 눌렀을 때
-            if (line < 9)
-            {
-                count = 5;
-                line--;
-                pos = line; // 엔터키 눌렀을 때 커서 이동
-            }
-            else
-            {
-                MessageBox(NULL, "10번이상 Enter금지.", "Warning", MB_OK);
-            }
-            break;
-        case VK_DOWN: // 아래 화살표 눌렀을 때
-            if (pos < line)
-                pos++; // 커서를 아래로 이동
-            break;
-        }
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
